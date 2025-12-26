cc642b3a3fdd7a186a5ccbf75ea87317ff69de8c
lmssee <lmssee@outlook.com>
2025-12-26 20:27:39 +0800

    version : v1.0.0 2025/12/26 20:27:39  依赖维护

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,31 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import cleanup from 'rollup-plugin-cleanup';
+// import terser from '@rollup/plugin-terser';
+// import cleanup from 'rollup-plugin-cleanup';
 import copy from 'rollup-plugin-copy';
 import { external } from '@qqi/rollup-external';
 
 export default {
-  input: './index.ts',
-  output: [
-    {
-      format: 'es',
-      entryFileNames: '[name].mjs',
-      preserveModules: false,
-      sourcemap: false,
-      exports: 'named',
-      dir: 'dist/',
-    },
-    //  若是生成 `bin` 类型，或是生成的文件不包含 commonJs，下面导出 commonJs 的配置可是删除
-    {
-      format: 'cjs',
-      entryFileNames: '[name].cjs',
-      preserveModules: false,
-      sourcemap: false,
-      exports: 'named',
-      dir: 'dist/',
-    },
-  ],
+  input: './src/index.ts',
+  output: ['es', 'cjs'].map(e => ({
+    format: e, // 打包模式
+    entryFileNames: '[name].js', // 输出文件名
+    preserveModules: true, //保留独立模块结构（关键）
+    preserveModulesRoot: 'src', // 保持 src 目录结构
+    sourcemap: false, // 关闭 source map
+    exports: 'named', // 导出模式
+    dir: `dist/${e}/`, // 输出文件夹
+  })),
   // 配置需要排除的包
   external: external(),
   plugins: [
     resolve(),
     commonjs(),
     typescript({}),
-    terser(),
-    cleanup(),
+    // 在使用的时候在进行压缩打包
+    // terser(),
+    // cleanup(),
     copy({
       targets: [
         { src: 'README.md', dest: 'dist' },
--- scripts/clean-package-json.js
+++ scripts/clean-package-json.js
@@ -1,40 +1,62 @@
 import {
-  getDirectoryBy,
   pathJoin,
   readFileToJsonSync,
+  getDirectoryBy,
   writeJsonFile,
 } from 'a-node-tools';
-let packageJson = readFileToJsonSync('./package.json');
 
-['scripts', 'devDependencies', 'lint-staged', 'private'].forEach(
-  e => delete packageJson[e],
-);
+// 原始 package.json 内容
+let packageJson = readFileToJsonSync('./package.json');
+const dependencies = packageJson.dependencies;
+// 移除冗余的键
+[
+  'scripts',
+  'devDependencies',
+  'lint-staged',
+  'private',
+  'dependencies',
+].forEach(key => delete packageJson[key]);
+const esPrefix = 'es'; // es 前缀
+const cjsPrefix = 'cjs'; // cjs 前缀
+const dtsPrefix = 'es/src'; // 类型文件的前缀
+// 查看当前打包 dist 文件路径
+const distParentPath = getDirectoryBy('dist', 'directory');
 
 packageJson = {
   ...packageJson,
-  main: 'index.cjs',
-  module: 'index.mjs',
-  types: 'index.d.ts',
-  files: ['index.mjs', 'index.cjs', 'index.d.ts', 'src'],
+  main: cjsPrefix + '/index.js', // 旧版本 CommonJs 入口
+  module: esPrefix + '/index.js', // 旧版本 ESM 入口
+  types: dtsPrefix + '/index.d.ts', // 旧版本类型入口
+  author: {
+    name: '泥豆君',
+    email: 'Mr.MudBean@outlook.com',
+    url: 'https://earthnut.dev',
+  },
+  description: '设置行内样式',
+  sideEffects: false, // 核心：开启 Tree Shaking
+  license: 'MIT',
+  files: [esPrefix, cjsPrefix, 'LICENSE', 'README.md'],
   exports: {
-    import: './index.mjs',
-    require: './index.cjs',
-    types: './index.d.ts',
+    import: `./${esPrefix}/index.js`,
+    default: `./${esPrefix}/index.js`,
+    require: `./${cjsPrefix}/index.js`,
+    types: `./${dtsPrefix}/index.d.ts`,
+  },
+  keywords: [
+    'element-inline-style',
+    'inline style',
+    '行内样式',
+    '简单写入样式',
+  ],
+  homepage: 'https://earthnut.dev/npm/a-element-inline-style',
+  dependencies,
+  bugs: {
+    url: 'https://github.com/MrMudBean/a-element-inline-style/issues',
+    email: 'Mr.MudBean@outlook.com',
   },
   repository: {
     type: 'git',
-    url: 'git+https://github.com/earthnutDev/a-element-inline-style.git',
-  },
-  keywords: ['element-inline-style', 'earthnut'],
-  author: {
-    name: '花生亻',
-    email: 'earthnut.dev@outlook.com',
-    url: 'https://earthnut.dev/about',
-  },
-  homepage: 'https://earthnut.dev/npm/a-element-inline-style',
-  bugs: {
-    url: 'https://github.com/earthnutDev/a-element-inline-style/issues',
-    email: 'earthnut.dev@outlook.com',
+    url: 'git+https://github.com/MrMudBean/a-element-inline-style.git',
   },
   publishConfig: {
     access: 'public',
@@ -42,15 +64,14 @@ packageJson = {
   },
   engines: {
     // globalThis 支持的最低版本的 node 为 12
-    node: '>=15.0.0',
+    node: '>=18.0.0',
   },
   browserslist: ['last 2 versions not ie <= 11'],
 };
 
 {
-  const distPath = getDirectoryBy('dist', 'directory');
-
-  const distPackagePath = pathJoin(distPath, './dist/package.json');
-
+  // 整理打包后 package.json 文件路径
+  const distPackagePath = pathJoin(distParentPath, './dist/package.json');
+  // 写入新的 packages.json 文件
   writeJsonFile(distPackagePath, packageJson);
 }
